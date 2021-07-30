278ed2e9d33afc65954e11fdd90372e8c3932304
Jusxin <72774910+Jusxin@users.noreply.github.com>
2021-07-29 22:48:35 -0400

    adding task function

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const addBtn = document.getElementById('add')
 
-
-addBtn.addEventListener('click', event => {
-    console.log("clicked");
-});
-
-
 const addToList = document.getElementById('list')
 
-function addItem(name) {
-    let li = document.createElement('li');
-    li.textContent = name;
-    return li;
-}
+let boxInput = document.getElementById('textInput').value;
 
-addToList.appendChild(addItem('isssss lit'));
+
+
+
+addBtn.addEventListener('click', function addItem(boxInput) {
+    boxInput = document.getElementById('textInput').value;
+    let li = document.createElement('li');
+    li.textContent = boxInput;
+    return addToList.appendChild(li);
+}
+)
+
+
+
+
+
+
+/*addBtn.addEventListener('click', function text() {
+    let boxInput = document.getElementById('textInput').value;
+
+    console.log(boxInput)
+});
+
+*/
+/*
+
+if (addBtn.addEventListener('click', function addItem(task) {
+    let li = document.createElement('li');
+    li.textContent = task;
+    
+    return addToList.appendChild(boxInput);
+
+    
+}
+));
+
+*/
