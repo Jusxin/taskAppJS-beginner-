8ca6158e12b8c0c102a7a2cb2b2bd9613c06cd3c
Jusxin <72774910+Jusxin@users.noreply.github.com>
2021-08-01 22:46:43 -0400

    some files fix

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const task = document.getElementById('list');
 addBtn.addEventListener('click', function addItem(boxInput) {
     boxInput = document.getElementById('textInput').value;
     let li = document.createElement('li');
-    li.id='task';
+    li.id = 'task';
     li.textContent = boxInput;
-    
+
     let btn = document.createElement('button');
-    btn.id='delBtn'
+    btn.id = 'delBtn'
     btn.textContent = 'Delete'
-    
+
     addToList.appendChild(li);
     addToList.appendChild(btn)
     //addToList.appendChild(btn);
@@ -28,24 +28,24 @@ toEmptyBox.addEventListener('click', function emptyBox() {
 
 )
 
-task.addEventListener('click', function delTask(e) { 
+task.addEventListener('click', function delTask(e) {
     if (e.target && e.target.nodeName == 'LI') {
-        (e.target).setAttribute('style', 'text-decoration-line: line-through; text-decoration-color: rgba(255,255,255, 0.6); text-decoration-thickness: 4px');  
+        (e.target).setAttribute('style', 'text-decoration-line: line-through; text-decoration-color: rgba(255,255,255, 0.6); text-decoration-thickness: 4px');
         /*(e.target).setAttribute('style', 'text-decoration-color: rgb(0,0,0)');
         (e.target).setAttribute('style', 'text-decoration-thickness: 3px');*/
-        
-    
-}
+
+
+    }
 })
 
 
 
-task.addEventListener('dblclick', function delTask(e) { 
+task.addEventListener('dblclick', function delTask(e) {
     if (e.target && e.target.nodeName == 'LI') {
         this.removeChild(e.target)
-        
-    
-}
+
+
+    }
 })
 
 
@@ -57,7 +57,7 @@ if (addBtn.addEventListener('click', function deleteItem() {
     btnnn.textContent = 'Delete'
     addToList.appendChild(btnnn);
         btnnn.addEventListener('click', function removeItem() {
-            
+
         })
 }
 )
