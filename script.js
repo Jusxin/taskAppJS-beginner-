6e4362b2f586019f3b927cebec72dc8ba10e87d3
Jusxin <72774910+Jusxin@users.noreply.github.com>
2021-08-03 22:57:20 -0400

    ...

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ const addToList = document.getElementById('list')
 const toEmptyBox = document.getElementById('textInput');
 const task = document.getElementById('list');
 
-
-addBtn.addEventListener('click', function addItem(boxInput) {
+addingItem = function addItem(boxInput) {
     boxInput = document.getElementById('textInput').value;
     let li = document.createElement('li');
     li.id = 'task';
     li.textContent = boxInput;
 
-    let btn = document.createElement('button');
+    let btn = document.createElement('button')
     btn.id = 'delBtn'
     btn.textContent = 'Delete'
 
     addToList.appendChild(li);
     addToList.appendChild(btn)
-    //addToList.appendChild(btn);
     toEmptyBox.value = '';
 }
-)
+
+addBtn.addEventListener('click', addingItem)
+
 
 toEmptyBox.addEventListener('click', function emptyBox() {
     toEmptyBox.value = '';
@@ -31,9 +31,6 @@ toEmptyBox.addEventListener('click', function emptyBox() {
 task.addEventListener('click', function delTask(e) {
     if (e.target && e.target.nodeName == 'LI') {
         (e.target).setAttribute('style', 'text-decoration-line: line-through; text-decoration-color: rgba(255,255,255, 0.6); text-decoration-thickness: 4px');
-        /*(e.target).setAttribute('style', 'text-decoration-color: rgb(0,0,0)');
-        (e.target).setAttribute('style', 'text-decoration-thickness: 3px');*/
-
 
     }
 })
@@ -50,16 +47,4 @@ task.addEventListener('dblclick', function delTask(e) {
 
 
 
-/*
-if (addBtn.addEventListener('click', function deleteItem() {
-    let btnnn = document.createElement('button');
-    btnnn.id='removeItem';
-    btnnn.textContent = 'Delete'
-    addToList.appendChild(btnnn);
-        btnnn.addEventListener('click', function removeItem() {
 
-        })
-}
-)
-);
-*/
